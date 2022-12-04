595ae3b65915de9bb4061723cd51cab30007a4a0
ganeshmande <119119655+ganeshmande@users.noreply.github.com>
2022-12-04 16:23:54 +0530

    Add files via upload

---
+++ HelloGanesh.java
@@ -0,0 +1,12 @@
+public class HelloGanesh
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello Ganesh");
+        System.out.println("Hello Ganesh");
+
+        System.out.print("Hello Ganesh \nHello Ganesh\n");
+        System.out.print("Hello Ganesh \n\nHello Ganesh");
+
+    }
+}
